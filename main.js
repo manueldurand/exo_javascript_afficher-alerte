0745e3561cc4b0d7ecc3d06053beacff868994c3
manuel_durand <manuel.durand@mailo.com>
2022-11-22 11:38:28 +0100

    correction, fonction appelée 2 fois

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.writeln('<div>');
 document.writeln('<label for="champ1">A vous :</label>');
 document.writeln('<input type="text" id="champ1">');
 document.writeln('</div>');
-document.writeln('<input type="submit" id="submit" onclick="(test_reponse())">')
+document.writeln('<input type="submit" id="submit">')
 document.writeln('</form>');
 document.getElementById("champ1").focus();
 document.addEventListener("submit", test_reponse);

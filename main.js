5826e492cf931994c715c19fe53057e7fa3e8a60
manuel_durand <manuel.durand@mailo.com>
2022-11-22 07:02:46 +0100

    pb affiché : function undefined mais le script fonctionne... a debugguer

---
+++ main.js
@@ -0,0 +1,21 @@
+document.writeln('<form id="form1">');
+document.writeln('<div>');
+document.writeln('<label for="champ1">A vous :</label>');
+document.writeln('<input type="text" id="champ1">');
+document.writeln('</div>');
+document.writeln('<input type="submit" id="submit" onclick="(test_reponse())">')
+document.writeln('</form>');
+document.getElementById("champ1").focus();
+document.addEventListener("submit", test_reponse);
+function test_reponse(){
+    let reponse = document.getElementById("champ1").value;
+    if (reponse.length == 0){
+        test_reponse.preventDefault;
+        alert("pensez à écrire quelquechose ...!");
+        document.getElementById("champ1").focus();
+    }
+    else{
+        alert("Ok,"+ " "+reponse + ", c'est noté")
+    }
+};
+
